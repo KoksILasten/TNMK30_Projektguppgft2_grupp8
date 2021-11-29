51f4f59b32c39424dddbda137fc4272234f960f4
Isakskott <isak.skott@gmail.com>
2021-11-29 19:44:26 +0100

    Help dropdown
    nästan fixad man fattar inte varför man behöver dubbelklicka??

--- Projektarbete/javaprojekt.js
+++ Projektarbete/javaprojekt.js
@@ -1,21 +1,24 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
-
 function dropdown() {
 
+
+
+
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
     for (i = 0; i < acc.length; i++) {
-        acc[i].addEventListener("click", function() {
+        acc[0].addEventListener("click", function() {
             /* Toggle between adding and removing the "active" class,
-            to highlight the button that controls the panel */
+    to highlight the button that controls the panel */
             this.classList.toggle("active");
 
             /* Toggle between hiding and showing the active panel */
             var panel = this.nextElementSibling;
-            if (panel.style.display === "block") {
-                panel.style.display = "none";
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
             } else {
-                panel.style.display = "block";
+                panel.style.maxHeight = panel.scrollHeight + "px";
             }
-        })
+        });
     }
 }
