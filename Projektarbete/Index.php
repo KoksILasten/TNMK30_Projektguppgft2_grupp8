7d4383b75a8a3c64c6cb2466689a539443058d06
Magnus <91549385+KoksILasten@users.noreply.github.com>
2021-12-03 16:59:35 +0100

    Index nästan klar

--- Projektarbete/Index.php
+++ Projektarbete/Index.php
@@ -1,30 +1,30 @@
-<?php include"meny.txt"; ?>
-  
+<?php include "meny.txt"; ?>
 
 
-    <div class="searchbar">
-        <form action="search_result.php" method="POST">
-            <input type="text" placeholder="Search for your parts here...">
-            <button class="search" type="submit"><i class="buttonsearch"></i></button>
-        </form>
-        <div class="help">
-            <button class="accordion" onclick="dropdown()">How to search</button>
 
-            <div class="panel">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet ultrices tellus. Etiam pharetra lectus at purus pharetra finibus. Phasellus at commodo nisi, sit amet convallis nibh. Duis et pretium metus. Sed malesuada dapibus
-                    risus, a venenatis ex placerat ac. Maecenas luctus magna sed sodales tempor. Nulla tincidunt ac odio ac auctor. Praesent ut nulla mi. Sed accumsan, nibh at semper tincidunt, turpis risus porttitor sem, quis fermentum nisl lorem accumsan
-                    magna. Curabitur venenatis, lectus et feugiat tempus, sapien ipsum elementum lectus, id eleifend sapien ex vitae libero. Duis nibh sapien, congue nec pulvinar eget, tincidunt ac arcu. Donec cursus volutpat risus ac semper. In tortor
-                    dolor, consectetur vitae porta nec, auctor ac lectus.</p>
-            </div>
+<div class="searchbar">
+    <form action="search_result.php" method="POST">
+        <input type="text" placeholder="Search for your parts here...">
+        <button class="search" type="submit"><i class="buttonsearch"></i></button>
+    </form>
+    <div class="help">
+        <button class="accordion" onclick="dropdown()">How to search</button>
 
+        <div id="myPanel" class="panel">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet ultrices tellus. Etiam pharetra lectus at purus pharetra finibus. Phasellus at commodo nisi, sit amet convallis nibh. Duis et pretium metus. Sed malesuada dapibus
+                risus, a venenatis ex placerat ac. Maecenas luctus magna sed sodales tempor. Nulla tincidunt ac odio ac auctor. Praesent ut nulla mi. Sed accumsan, nibh at semper tincidunt, turpis risus porttitor sem, quis fermentum nisl lorem accumsan
+                magna. Curabitur venenatis, lectus et feugiat tempus, sapien ipsum elementum lectus, id eleifend sapien ex vitae libero. Duis nibh sapien, congue nec pulvinar eget, tincidunt ac arcu. Donec cursus volutpat risus ac semper. In tortor
+                dolor, consectetur vitae porta nec, auctor ac lectus.</p>
         </div>
-    </div>
-
-
-    <div id="bg-img">
-        <div class="blur-size"></div>
 
     </div>
+</div>
+
+
+<div id="bg-img">
+    <div class="blur-size"></div>
+
+</div>
 
 
 
--- Projektarbete/javaprojekt.js
+++ Projektarbete/javaprojekt.js
@@ -1,24 +1,25 @@
 function dropdown() {
+  var acc = document.getElementsByClassName("accordion");
+  //  var i = 0;
+  acc[0].classList.toggle("active");
 
+  let panel = document.getElementById("myPanel");
+  if (panel.style.maxHeight) {
+    panel.style.maxHeight = "0px";
+  } else {
+    panel.style.maxHeight = panel.scrollHeight + "px";
+  }
 
+  acc[0].addEventListener("click", function () {
+    //       /* Toggle between adding and removing the "active" class,
+    //     to highlight the button that controls the panel */
 
-
-    var acc = document.getElementsByClassName("accordion");
-    var i;
-
-    for (i = 0; i < acc.length; i++) {
-        acc[0].addEventListener("click", function() {
-            /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-            this.classList.toggle("active");
-
-            /* Toggle between hiding and showing the active panel */
-            var panel = this.nextElementSibling;
-            if (panel.style.maxHeight) {
-                panel.style.maxHeight = null;
-            } else {
-                panel.style.maxHeight = panel.scrollHeight + "px";
-            }
-        });
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
     }
+  });
 }
