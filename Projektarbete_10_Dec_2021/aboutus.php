c49566fb380160313e099c7999b9f8ea5d301580
adhamkteileh01 <91637152+adhamkteileh01@users.noreply.github.com>
2021-12-10 12:22:52 +0100

    aboutus AK 10/12

--- Projektarbete_10_Dec_2021/aboutus.php
+++ Projektarbete_10_Dec_2021/aboutus.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lego Search</title>
-    <link href="aboutus_style.css" media="screen" rel="stylesheet" type="text/css" />
+    <link href="aboutus_style.css" media="screen" rel="stylesheet" type="text/css" /> 
     <script src="javaprojekt.js"></script>
 </head>
 
@@ -25,11 +25,7 @@
     </div>
 
 
-<div class="about-section">
-  <h1>About Us Page</h1>
-  <p>Some text about who we are and what we do.</p>
-  <p>Resize the browser window to see that this page is responsive by the way.</p>
-</div>
+
 
 <h2 style="text-align:center">Our Team</h2>
 <div class="column">
