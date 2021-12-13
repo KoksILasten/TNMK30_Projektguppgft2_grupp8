0c57f9699d3dcdb673307b1ea406de08651317cd
Magnus <91549385+KoksILasten@users.noreply.github.com>
2021-12-13 15:01:35 +0100

    weg

--- Projekt 21-12-10/Projektarbete/help.php
+++ Projekt 21-12-10/Projektarbete/help.php
@@ -2,34 +2,34 @@
 <html>
 
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lego Search</title>
-   	<link href="help.css" media="screen" rel="stylesheet" type="text/css" />
-    <script src="javaprojekt.js"></script>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Lego Search</title>
+	<link href="help.css" media="screen" rel="stylesheet" type="text/css" />
+	<script src="javaprojekt.js"></script>
 </head>
 
 <body>
-  <div class="headernavbar">
-        <ul id="header">
+	<div class="headernavbar">
+		<ul id="header">
 
 
-            <li class="text"><a href="aboutus.php"> About Us</a></li>
-            <li id="lego">
-                <a href="Index.php"><img src="legobit.png" alt="Picture couldn´t be laoded"></a>
-            </li>
-            <li class="text"><a href="help.php">Help</a></li>
+			<li class="text"><a href="aboutsus.php"> About Us</a></li>
+			<li id="lego">
+				<a href="Index.php"><img src="legobit.png" alt="Picture couldn´t be laoded"></a>
+			</li>
+			<li class="text"><a href="help.php">Help</a></li>
 
 
-        </ul>
-    </div>
+		</ul>
+	</div>
 
-	
-	
+
+
 	<!-- Body of help page content-->
 	<div class="help_body">
-		 <h1> Welcome to our Help Page!</h1>	
-		
+		<h1> Welcome to our Help Page!</h1>
+
 		<div class="helpsection">
 			<h2> We will, we will, help you! </h2>
 			<h4> How to search for your parts! </h4>
@@ -37,27 +37,28 @@
 			<img class="images" src="help_click.png" alt="help_picture_1">
 			<p> You will then get a summarised text about how to search</p>
 			<img class="images" src="help_clicked.png" alt="help_picture_2">
-			
+
 			<h4> What can I search for? </h4>
 			<p> You can search on anything from:</p>
 			<p> Individual pieces</p>
 			<p> Sets </p>
 			<p> and more!</p>
-			
+
 			<h4> How can I contact the developers of this magnificent website? </h4>
 			<p> Press on "About us"</p>
 			<img class="images" src="insert pic" alt="help_picture_3">
 			<p> Here you will be able to find all sorts of info about them!</p>
 		</div>
-		
+
 	</div>
 
 	<!-- Background picture -->
 	<div id="bg-img">
-        <div class="blur-size"></div>
+		<div class="blur-size"></div>
 	</div>
-	
-	
-	
+
+
+
 </body>
+
 </html>
